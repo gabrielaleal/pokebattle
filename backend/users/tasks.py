b8a3aac3a9d233af52d14277dda504a7cd685a43
gabriela <gabriela.leal@hotmail.com>
2020-02-19 15:49:44 -0300

    modify

--- backend/users/tasks.py
+++ backend/users/tasks.py
@@ -1,8 +1,8 @@
 from django.core import management
 
-from pokebattle import celery_app
+from backend.pokebattle import celery_app
 
 
 @celery_app.task
 def clearsessions():
-    management.call_command('clearsessions')
+    management.call_command("clearsessions")
