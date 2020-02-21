8a2868d215c2740e3af9a408fd04950446c7edf0
gabriela <gabriela.leal@hotmail.com>
2020-02-21 16:42:35 -0300

    update template file path

--- backend/battles/views.py
+++ backend/battles/views.py
@@ -8,10 +8,13 @@ from .models import Battle  # noqa
 # Create your views here.
 class CreateBattleView(generic.CreateView):  # noqa
     model = Battle
-    template_name = "battles/create.html"
+    template_name = "create_battle.html"
     form_class = CreateBattleForm
     success_url = "/"
 
     def form_valid(self, form):
         form.instance.creator = self.request.user
         return super().form_valid(form)
+
+    def get_initial(self):
+        return {"creator": self.request.user.id}
