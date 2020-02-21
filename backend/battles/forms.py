f2f279ab48f27d694bd110695568ef40c1f4d3cb
gabriela <gabriela.leal@hotmail.com>
2020-02-21 16:40:53 -0300

    filter opponents list excluind the request user

--- backend/battles/forms.py
+++ backend/battles/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 
+from users.models import User  # noqa
+
 from .models import Battle  # noqa
 
 
@@ -7,3 +9,7 @@ class CreateBattleForm(forms.ModelForm):
     class Meta:
         model = Battle
         fields = ["opponent"]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["opponent"].queryset = User.objects.exclude(id=self.initial["creator"])
