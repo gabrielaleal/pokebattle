b2c9f11399c812035d438568217f39d4b26592f0
Gabriela Leal <gabriela.leal@hotmail.com>
2020-02-28 11:45:21 -0300

    treat data coming from form

--- backend/battles/views.py
+++ backend/battles/views.py
@@ -1,11 +1,12 @@
 # from django.shortcuts import render
 from django.views import generic
 
+from pokemon.helpers import save_pokemon  # noqa
+
 from .forms import CreateBattleForm  # noqa
-from .models import Battle  # noqa
+from .models import Battle, BattleTeam  # noqa
 
 
-# Create your views here.
 class CreateBattleView(generic.CreateView):  # noqa
     model = Battle
     template_name = "create_battle.html"
@@ -14,6 +15,17 @@ class CreateBattleView(generic.CreateView):  # noqa
 
     def form_valid(self, form):
         form.instance.creator = self.request.user
+        form.instance.save()
+
+        pokemon = {}
+
+        for field in ["pokemon_1", "pokemon_2", "pokemon_3"]:
+            pokemon[field] = save_pokemon(form.cleaned_data[field])
+
+        BattleTeam.objects.create(
+            creator=self.request.user, battle=form.instance, **pokemon
+        )  # TODO: revisit this later
+
         return super().form_valid(form)
 
     def get_initial(self):
