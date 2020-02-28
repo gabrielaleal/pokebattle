0655a61a39cb45e4377bdfe7984ebe21ef159d4d
Gabriela Leal <gabriela.leal@hotmail.com>
2020-02-28 10:25:45 -0300

    create pokemon model

--- backend/pokemon/models.py
+++ backend/pokemon/models.py
@@ -1,4 +1,8 @@
 from django.db import models  # noqa
 
 
-# Create your models here.
+class Pokemon(models.Model):
+    name = models.CharField(max_length=50)
+    attack = models.IntegerField()
+    defense = models.IntegerField()
+    hitpoints = models.IntegerField()
