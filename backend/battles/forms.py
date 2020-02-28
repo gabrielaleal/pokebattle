d625d1c2a8cc2dc6437c87d27c3c4c623d2ba5e2
Gabriela Leal <gabriela.leal@hotmail.com>
2020-02-28 11:47:13 -0300

    add pokemon fields

--- backend/battles/forms.py
+++ backend/battles/forms.py
@@ -6,6 +6,10 @@ from .models import Battle  # noqa
 
 
 class CreateBattleForm(forms.ModelForm):
+    pokemon_1 = forms.CharField(required=True)
+    pokemon_2 = forms.CharField(required=True)
+    pokemon_3 = forms.CharField(required=True)
+
     class Meta:
         model = Battle
         fields = ["opponent"]
@@ -13,3 +17,10 @@ class CreateBattleForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields["opponent"].queryset = User.objects.exclude(id=self.initial["creator_id"])
+
+    def clean(self):
+        cleaned_data = super().clean()
+
+        # validation logic here
+
+        return cleaned_data
