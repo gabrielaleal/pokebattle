573162a5f23ee106ad262d2f494b85e2ca3d126b
gabriela <gabriela.leal@hotmail.com>
2020-03-01 15:45:13 -0300

    treat response from api to return errors

--- backend/battles/forms.py
+++ backend/battles/forms.py
@@ -1,14 +1,15 @@
 from django import forms
 
+from pokemon.helpers import get_pokemon  # noqa
 from users.models import User  # noqa
 
 from .models import Battle  # noqa
 
 
 class CreateBattleForm(forms.ModelForm):
-    pokemon_1 = forms.CharField(required=True)
-    pokemon_2 = forms.CharField(required=True)
-    pokemon_3 = forms.CharField(required=True)
+    pokemon_1 = forms.IntegerField(required=True)
+    pokemon_2 = forms.IntegerField(required=True)
+    pokemon_3 = forms.IntegerField(required=True)
 
     class Meta:
         model = Battle
@@ -21,6 +22,10 @@ class CreateBattleForm(forms.ModelForm):
     def clean(self):
         cleaned_data = super().clean()
 
-        # validation logic here
+        for field in ["pokemon_1", "pokemon_2", "pokemon_3"]:
+            response = get_pokemon(self.cleaned_data[field])  # requests from the API
+
+            if response.status_code != 200:  # in case the pokemon does not exist on the API
+                self.add_error(field, "Sorry, we couldn't find this Pokemon.")
 
         return cleaned_data
