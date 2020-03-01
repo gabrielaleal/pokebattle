0c3777f1fd1b117522d44f105aaaa849c898f142
gabriela <gabriela.leal@hotmail.com>
2020-03-01 15:45:57 -0300

    update api return data on get_pokemon method

--- backend/pokemon/helpers.py
+++ backend/pokemon/helpers.py
@@ -7,24 +7,27 @@ POKE_API_URL = "https://pokeapi.co/api/v2/pokemon/"
 
 
 def get_pokemon(poke_id):
-    response = requests.get(f"{POKE_API_URL}{poke_id.lower()}")
-    data = response.json()
+    response = requests.get(f"{POKE_API_URL}{poke_id}")
+    # data = response.json()
     # response.status_code may be useful to treat data
-    return data
+    return response
 
 
 def save_pokemon(poke_id):
-    data = get_pokemon(poke_id)
+    pokemon = Pokemon.objects.filter(poke_id=poke_id).first()  # checks if pokemon already exists
 
-    pokemon = Pokemon.objects.filter(
-        id__iexact=poke_id
-    ).first()  # __iexect makes poke_id case insensitive (database lookups)
-
-    if pokemon:
+    if pokemon:  # if it does, return it
         return pokemon
+
+    response = get_pokemon(poke_id)  # otherwise, request this new pokemon
+
+    data = response.json()
 
     defense = data["stats"][3]["base_stat"]
     attack = data["stats"][4]["base_stat"]
     hp = data["stats"][5]["base_stat"]
 
-    return Pokemon.objects.create(name=data["name"], defense=defense, attack=attack, hitpoints=hp)
+    # save and return new pokemon:
+    return Pokemon.objects.create(
+        poke_id=poke_id, name=data["name"], defense=defense, attack=attack, hitpoints=hp
+    )
