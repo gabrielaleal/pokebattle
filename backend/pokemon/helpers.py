ab24eb47a8e827849b38f7f3918b871cc915683f
Gabriela Leal <gabriela.leal@hotmail.com>
2020-03-02 14:44:24 -0300

    add method to validate pokemon sum

--- backend/pokemon/helpers.py
+++ backend/pokemon/helpers.py
@@ -9,7 +9,12 @@ POKE_API_URL = "https://pokeapi.co/api/v2/pokemon/"
 def get_pokemon(poke_id):
     response = requests.get(f"{POKE_API_URL}{poke_id}")
     data = response.json()
-    return data
+    return {
+        "name": data["name"],
+        "defense": data["stats"][3]["base_stat"],
+        "attack": data["stats"][4]["base_stat"],
+        "hp": data["stats"][5]["base_stat"],
+    }
 
 
 def pokemon_exists(poke_id):
@@ -25,11 +30,26 @@ def save_pokemon(poke_id):
 
     data = get_pokemon(poke_id)  # otherwise, request this new pokemon
 
-    defense = data["stats"][3]["base_stat"]
-    attack = data["stats"][4]["base_stat"]
-    hp = data["stats"][5]["base_stat"]
-
     # save and return new pokemon:
     return Pokemon.objects.create(
-        poke_id=poke_id, name=data["name"], defense=defense, attack=attack, hitpoints=hp
+        poke_id=poke_id,
+        name=data["name"],
+        defense=data["defense"],
+        attack=data["attack"],
+        hp=data["hp"],
     )
+
+
+def pokemon_sum_valid(pokemon_ids):
+    poke_sum = 0
+    for poke_id in pokemon_ids:
+        pokemon = Pokemon.objects.filter(poke_id=poke_id).first()
+
+        if pokemon:
+            poke_sum += pokemon.attack + pokemon.defense + pokemon.hp
+            continue
+
+        pokemon = get_pokemon(poke_id)
+        poke_sum += pokemon["attack"] + pokemon["defense"] + pokemon["hp"]
+
+    return poke_sum <= 600
