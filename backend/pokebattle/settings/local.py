fb9f115dbdfb524bbbe3fd5c2788b86fc917912f
Gabriela Leal <gabriela.leal@hotmail.com>
2020-03-03 15:49:28 -0300

    set local database

--- backend/pokebattle/settings/local.py
+++ backend/pokebattle/settings/local.py
@@ -1,1 +1,7 @@
 from .local_base import *  # noqa
+
+from dj_database_url import parse as db_url
+
+DATABASES = {
+    "default": db_url("postgresql://postgres:postgres@localhost:5432/pokebattle"),
+}
