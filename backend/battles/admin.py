bf09efa8812af064c8db476b3091a688d7310080
gabriela <gabriela.leal@hotmail.com>
2020-03-04 21:35:06 -0300

    update functions names

--- backend/battles/admin.py
+++ backend/battles/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .battle import run_battle  # noqa
+from .battle import run_battle_and_send_result_email  # noqa
 from .models import Battle, BattleTeam  # noqa
 
 
@@ -11,7 +11,7 @@ class BattleAdmin(admin.ModelAdmin):
 class BattleTeamAdmin(admin.ModelAdmin):
     def save_model(self, request, obj, form, change):
         super().save_model(request, obj, form, change)
-        run_battle(obj)
+        run_battle_and_send_result_email(obj)
 
 
 admin.site.register(Battle, BattleAdmin)
--- backend/battles/battle.py
+++ backend/battles/battle.py
@@ -1,11 +1,12 @@
 from .email import send_battle_result  # noqa
 
 
-def win_attack(pokemon_1, pokemon_2):  # pokemon_1 always attacks and pokemon_2 always defends
+def compare_pokemon_attack_and_defense(pokemon_1, pokemon_2):
+    # pokemon_1 always attacks and pokemon_2 always defends
     return pokemon_1.attack > pokemon_2.defense
 
 
-def highest_hp_pokemon(pokemon_1, pokemon_2):
+def get_pokemon_with_highest_hp(pokemon_1, pokemon_2):
     if pokemon_1.hp > pokemon_2.hp:
         return pokemon_1
     return pokemon_2
@@ -13,31 +14,30 @@ def highest_hp_pokemon(pokemon_1, pokemon_2):
 
 def get_battle_result(battle):
     creator_team = battle.creator.teams.filter(battle=battle).first()
-    opponent_team = battle.creator.teams.filter(battle=battle).first()
+    opponent_team = battle.opponent.teams.filter(battle=battle).first()
 
     # first two rounds:
-    if win_attack(creator_team.pokemon_1, opponent_team.pokemon_1) and win_attack(
-        creator_team.pokemon_2, opponent_team.pokemon_2
-    ):
+    if compare_pokemon_attack_and_defense(
+        creator_team.pokemon_1, opponent_team.pokemon_1
+    ) and compare_pokemon_attack_and_defense(creator_team.pokemon_2, opponent_team.pokemon_2):
         return battle.creator
 
-    if win_attack(opponent_team.pokemon_1, creator_team.pokemon_1) and win_attack(
-        opponent_team.pokemon_2, creator_team.pokemon_2
-    ):
+    if compare_pokemon_attack_and_defense(
+        opponent_team.pokemon_1, creator_team.pokemon_1
+    ) and compare_pokemon_attack_and_defense(opponent_team.pokemon_2, creator_team.pokemon_2):
         return battle.opponent
 
     # in case of draw:
     if (
-        highest_hp_pokemon(creator_team.pokemon_3, opponent_team.pokemon_3)
+        get_pokemon_with_highest_hp(creator_team.pokemon_3, opponent_team.pokemon_3)
         == creator_team.pokemon_3
     ):
         return battle.creator
     return battle.opponent
 
 
-def run_battle(battle_team):
+def run_battle_and_send_result_email(battle_team):
     battle = battle_team.battle
     battle.winner = get_battle_result(battle)
     battle.save()
     send_battle_result(battle)
-    print("done!")
