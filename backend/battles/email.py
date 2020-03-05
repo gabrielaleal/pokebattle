c1ab168305e5cd85b9cf04261ba9a6fc2dcdd1e3
gabriela <gabriela.leal@hotmail.com>
2020-03-04 21:36:35 -0300

    remove comments

--- backend/battles/email.py
+++ backend/battles/email.py
@@ -1,11 +1,12 @@
+from django.conf import settings
+
 from templated_email import send_templated_mail  # noqa
 
 
 def send_battle_result(battle):
-    # import ipdb;ipdb.set_trace();
     send_templated_mail(
         template_name="battle_result",
-        from_email="gabriela@vinta.com.br",
+        from_email=settings.EMAIL_ADDRESS,
         recipient_list=[battle.creator.email, battle.opponent.email],
         context={
             "battle_creator": battle.creator.email,
@@ -13,10 +14,4 @@ def send_battle_result(battle):
             "battle_winner": battle.winner.email,
             "battle_id": battle.id,
         },
-        # Optional:
-        # cc=['cc@example.com'],
-        # bcc=['bcc@example.com'],
-        # headers={'My-Custom-Header':'Custom Value'},
-        # template_prefix="my_emails/",
-        # template_suffix="email",
     )
