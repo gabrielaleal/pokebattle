92b88163952c227fd2636ced9a6da9633cdcc5cc
gabriela <gabriela.leal@hotmail.com>
2020-03-04 21:33:30 -0300

    remove from version control

--- backend/pokebattle/settings/local.py
+++
@@ -1,7 +0,0 @@
-from .local_base import *  # noqa
-
-from dj_database_url import parse as db_url
-
-DATABASES = {
-    "default": db_url("postgresql://postgres:postgres@localhost:5432/pokebattle"),
-}
