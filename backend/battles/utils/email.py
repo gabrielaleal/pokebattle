b324587eea08bdd2e9d9bef3f7e43c33b159d64f
gabriela <gabriela.leal@hotmail.com>
2020-03-07 16:09:42 -0300

    create an utils folder

--- backend/battles/utils/email.py
+++ backend/battles/utils/email.py
@@ -4,14 +4,13 @@ from templated_email import send_templated_mail  # noqa
 
 
 def send_battle_result(battle):
-    # import ipdb; ipdb.set_trace()
     send_templated_mail(
         template_name="battle_result",
         from_email=settings.EMAIL_ADDRESS,
         recipient_list=[battle.creator.email, battle.opponent.email],
         context={
             "battle_creator": battle.creator.email,
-            "battle_oponent": battle.opponent.email,
+            "battle_opponent": battle.opponent.email,
             "battle_winner": battle.winner.email,
             "battle_id": battle.id,
             "creator_team": battle.creator.teams.filter(battle=battle.id).first(),
