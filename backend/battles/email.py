7f12bd68235fe1eeedad5004e566c218c230d996
gabriela <gabriela.leal@hotmail.com>
2020-03-07 14:35:39 -0300

    add more info for email template

--- backend/battles/email.py
+++ backend/battles/email.py
@@ -4,6 +4,7 @@ from templated_email import send_templated_mail  # noqa
 
 
 def send_battle_result(battle):
+    # import ipdb; ipdb.set_trace()
     send_templated_mail(
         template_name="battle_result",
         from_email=settings.EMAIL_ADDRESS,
@@ -13,5 +14,7 @@ def send_battle_result(battle):
             "battle_oponent": battle.opponent.email,
             "battle_winner": battle.winner.email,
             "battle_id": battle.id,
+            "creator_team": battle.creator.teams.filter(battle=battle.id).first(),
+            "opponent_team": battle.opponent.teams.filter(battle=battle.id).first(),
         },
     )
