8615bdf1169e92b78d623f5679c80b56f0b9469e
Gabriela Leal <gabriela.leal@hotmail.com>
2020-03-09 14:06:32 -0300

    create settled battle list path

--- backend/battles/urls.py
+++ backend/battles/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
 
-from .views import CreateBattleView  # noqa
+from .views import CreateBattleView, SettledBattlesListView  # noqa
 
 
 app_name = "battles"
 
 urlpatterns = [
     url(r"^create/$", CreateBattleView.as_view(), name="create-battle"),
+    url(r"^settled-battles/$", SettledBattlesListView.as_view(), name="settled-battles-list"),
 ]
