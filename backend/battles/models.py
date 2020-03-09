63a0b9d40aaa6fe715d4f31947e34f10d9b2168c
Gabriela Leal <gabriela.leal@hotmail.com>
2020-03-09 14:05:41 -0300

    add battle's new attribute: status

--- backend/battles/models.py
+++ backend/battles/models.py
@@ -7,6 +7,7 @@ from users.models import User  # noqa
 class Battle(models.Model):
     creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name="battles_as_creator")
     opponent = models.ForeignKey(User, on_delete=models.CASCADE, related_name="battles_as_opponent")
+    status = models.CharField(max_length=20, null=True)
     winner = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="won_battles", null=True
     )
