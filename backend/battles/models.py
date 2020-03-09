8ce037bb1c629c5a69a943ca7017f93e06800cb9
Gabriela Leal <gabriela.leal@hotmail.com>
2020-03-09 15:48:02 -0300

    add battle's new attr: timestamp

--- backend/battles/models.py
+++ backend/battles/models.py
@@ -5,6 +5,7 @@ from users.models import User  # noqa
 
 
 class Battle(models.Model):
+    timestamp = models.DateTimeField(auto_now_add=True, null=True)
     creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name="battles_as_creator")
     opponent = models.ForeignKey(User, on_delete=models.CASCADE, related_name="battles_as_opponent")
     status = models.CharField(max_length=20, null=True)
