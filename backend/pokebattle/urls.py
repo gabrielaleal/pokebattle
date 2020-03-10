4064873860007bae55fa9528a660fe4098476234
Gabriela Leal <gabriela.leal@hotmail.com>
2020-03-10 16:38:51 -0300

    create sign up path

--- backend/pokebattle/urls.py
+++ backend/pokebattle/urls.py
@@ -4,10 +4,13 @@ from django.views.generic import TemplateView
 
 import django_js_reverse.views
 
+from users.views import SignUpView
+
 
 urlpatterns = [
     url(r"^admin/", admin.site.urls),
     url(r"^jsreverse/$", django_js_reverse.views.urls_js, name="js_reverse"),
     url(r"^$", TemplateView.as_view(template_name="home.html"), name="home"),
     url(r"^battles/", include("battles.urls"), name="battles"),  # import all urls from battles app
+    url(r"^signup/", SignUpView.as_view(), name="signup"),
 ]
