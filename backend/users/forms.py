cf6f1e6135a30fa52418834c15eccd794ee5be1f
Gabriela Leal <gabriela.leal@hotmail.com>
2020-03-11 16:19:31 -0300

    remove commented code

--- backend/users/forms.py
+++ backend/users/forms.py
@@ -8,11 +8,3 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ["email"]
-
-    # def clean(self):
-    #     cleaned_data = super().clean()
-    #     user_exists = User.objects.filter(email=self.cleaned_data["email"])
-    #     if user_exists:
-    #         raise forms.ValidationError(
-    #             "Ops, the email you entered is already registered."
-    # )
