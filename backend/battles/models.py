5e3886b2420d3ab23e6be241b26d593cbbbba6af
gabriela <gabriela.leal@hotmail.com>
2020-03-16 09:25:06 -0300

    use choices on Battle's status field

--- backend/battles/models.py
+++ backend/battles/models.py
@@ -4,11 +4,14 @@ from pokemon.models import Pokemon
 from users.models import User
 
 
+STATUS_CHOICES = [("ONGOING", "ONGOING"), ("SETTLED", "SETTLED")]
+
+
 class Battle(models.Model):
     timestamp = models.DateTimeField(auto_now_add=True, null=True)
     creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name="battles_as_creator")
     opponent = models.ForeignKey(User, on_delete=models.CASCADE, related_name="battles_as_opponent")
-    status = models.CharField(max_length=20, blank=True)
+    status = models.CharField(choices=STATUS_CHOICES, default="ONGOING", max_length=20, blank=True)
     winner = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="won_battles", null=True
     )
