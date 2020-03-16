5ca11b9ea27c2c4c5ca25fa80d73dd01fa2ab03f
gabriela <gabriela.leal@hotmail.com>
2020-03-16 10:48:15 -0300

    try to login user after signup

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,5 +1,5 @@
-from django.contrib import messages  # noqa
-from django.contrib.auth import authenticate, login  # noqa
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
 from django.urls import reverse_lazy
 from django.views import generic
 
@@ -8,17 +8,17 @@ from .forms import SignUpForm
 
 class SignUpView(generic.CreateView):
     form_class = SignUpForm
-    success_url = reverse_lazy("home")
+    success_url = reverse_lazy("signup")
     template_name = "signup.html"
 
-    # def form_valid(self, form):
-    #     new_user = form.save()
-    #     messages.info(self.request, "Thanks for registering. You are now logged in.")
-    #     new_user = authenticate(
-    #         email=form.cleaned_data["email"], password=form.cleaned_data["password1"],
-    #     )
-    #     if new_user is not None:
-    #         login(self.request, new_user)
-    #     else:
-    #         messages.info(request, "Sorry, did you register correctly?")
-    #     return super().form_valid(form)
+    def form_valid(self, form):
+        new_user = form.save()
+        new_user = authenticate(
+            email=form.cleaned_data["email"], password=form.cleaned_data["password1"],
+        )
+        if new_user is not None:
+            login(self.request, new_user)
+            messages.info(self.request, "Thanks for registering. You are now logged in.")
+        else:
+            messages.info(self.request, "Sorry, did you register correctly?")
+        return super().form_valid(form)
