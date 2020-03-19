0f72b6fe0965063ed9f6301dd5bec413a3a68c9d
gabriela <gabriela.leal@hotmail.com>
2020-03-19 15:03:51 -0300

    create settled battle detail path

--- backend/battles/urls.py
+++ backend/battles/urls.py
@@ -1,6 +1,11 @@
 from django.conf.urls import url
 
-from .views import CreateBattleView, OnGoingBattlesListView, SettledBattlesListView
+from .views import (
+    CreateBattleView,
+    OnGoingBattlesListView,
+    SettledBattleDetailView,
+    SettledBattlesListView,
+)
 
 
 app_name = "battles"
@@ -9,4 +14,5 @@ urlpatterns = [
     url(r"^create/$", CreateBattleView.as_view(), name="create-battle"),
     url(r"^settled-battles/$", SettledBattlesListView.as_view(), name="settled-battles-list"),
     url(r"^ongoing-battles/$", OnGoingBattlesListView.as_view(), name="ongoing-battles-list"),
+    url(r"^(?P<pk>[-\w]+)/$", SettledBattleDetailView.as_view(), name="settled-battle-detail"),
 ]
