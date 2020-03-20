b1096e1e21bd996862edf129d417d666364e16ff
gabriela <gabriela.leal@hotmail.com>
2020-03-20 13:10:13 -0300

    add img_url to Pokemon model

--- backend/pokemon/models.py
+++ backend/pokemon/models.py
@@ -4,6 +4,7 @@ from django.db import models  # noqa
 class Pokemon(models.Model):
     poke_id = models.IntegerField(verbose_name="PokeAPI ID")
     name = models.CharField(max_length=50)
+    img_url = models.CharField(max_length=100, blank=True)
     attack = models.IntegerField()
     defense = models.IntegerField()
     hp = models.IntegerField()
