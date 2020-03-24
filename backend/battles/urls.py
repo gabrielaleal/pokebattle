6e28791ff7f2c1d3c326b5a7a894ac1b45cbadfb
gabrielaleal <gabriela.leal@hotmail.com>
2020-03-24 09:53:43 -0300

    declare select battle team view path

--- backend/battles/urls.py
+++ backend/battles/urls.py
@@ -4,6 +4,7 @@ from .views import (
     BattleDetailView,
     CreateBattleView,
     OnGoingBattlesListView,
+    SelectOpponentTeamView,
     SettledBattlesListView,
 )
 
@@ -12,6 +13,7 @@ app_name = "battles"
 
 urlpatterns = [
     url(r"^create/$", CreateBattleView.as_view(), name="create-battle"),
+    url(r"^select-team/(?P<pk>[-\w]+)/$", SelectOpponentTeamView.as_view(), name="select-team"),
     url(r"^settled-battles/$", SettledBattlesListView.as_view(), name="settled-battles-list"),
     url(r"^ongoing-battles/$", OnGoingBattlesListView.as_view(), name="ongoing-battles-list"),
     url(r"^(?P<pk>[-\w]+)/$", BattleDetailView.as_view(), name="battle-detail"),
