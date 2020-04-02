41a0b70376848a58200ac81a00d063c62c02101b
gabrielaleal <gabriela.leal@hotmail.com>
2020-04-02 11:49:10 -0300

    update battle result email

--- backend/battles/utils/email.py
+++ backend/battles/utils/email.py
@@ -5,17 +5,20 @@ from templated_email import send_templated_mail
 
 
 def send_battle_result(battle):
+    battle_detail_path = reverse_lazy("battles:battle-detail", args=(battle.pk,))
+    battle_details_url = f"{settings.HOST}{battle_detail_path}"
     send_templated_mail(
         template_name="battle_result",
         from_email=settings.EMAIL_ADDRESS,
         recipient_list=[battle.creator.email, battle.opponent.email],
         context={
-            "battle_creator": battle.creator.email,
-            "battle_opponent": battle.opponent.email,
-            "battle_winner": battle.winner.email,
+            "battle_creator": battle.creator.email.split("@")[0],
+            "battle_opponent": battle.opponent.email.split("@")[0],
+            "battle_winner": battle.winner.email.split("@")[0],
             "battle_id": battle.id,
             "creator_team": battle.creator.teams.filter(battle=battle.id).first(),
             "opponent_team": battle.opponent.teams.filter(battle=battle.id).first(),
+            "battle_details_url": battle_details_url,
         },
     )
 
@@ -29,7 +32,7 @@ def send_opponent_battle_invitation_email(battle):
         recipient_list=[battle.opponent.email],
         context={
             "battle_id": battle.id,
-            "battle_creator": battle.creator.email,
+            "battle_creator": battle.creator.email.split("@")[0],
             "battle_opponent": battle.opponent.email.split("@")[0],
             "select_battle_team_url": select_battle_team_url,
         },
