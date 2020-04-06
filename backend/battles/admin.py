afbe32bb354545b3a249e08b547b289f70e79983
gabrielaleal <gabriela.leal@hotmail.com>
2020-04-06 12:14:14 -0300

    call task instead of method to run the battle

--- backend/battles/admin.py
+++ backend/battles/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from battles.utils.battle import run_battle_and_send_result_email
+from battles.tasks import run_battle_and_send_result_email
 
 from .models import Battle, BattleTeam
 
@@ -12,7 +12,7 @@ class BattleAdmin(admin.ModelAdmin):
 class BattleTeamAdmin(admin.ModelAdmin):
     def save_model(self, request, obj, form, change):
         super().save_model(request, obj, form, change)
-        run_battle_and_send_result_email(obj)
+        run_battle_and_send_result_email.delay(obj.battle.id)
 
 
 admin.site.register(Battle, BattleAdmin)
