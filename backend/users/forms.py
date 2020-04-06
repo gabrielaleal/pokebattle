e26598b4c9caecdb7999c47bf3ea122373ed0211
gabrielaleal <gabriela.leal@hotmail.com>
2020-04-06 14:57:44 -0300

    create invite user form

--- backend/users/forms.py
+++ backend/users/forms.py
@@ -1,7 +1,13 @@
-# from django import forms
+from django import forms
 from django.contrib.auth.forms import UserCreationForm
 
 from users.models import User
+
+
+class InviteUserForm(forms.Form):
+    email = forms.CharField()
+
+    # TODO: check if the user already exists
 
 
 class SignUpForm(UserCreationForm):
