990287146eda82fc366abd2f9e4b05d63a151780
gabrielaleal <gabriela.leal@hotmail.com>
2020-04-07 10:23:59 -0300

    add validation to 'invite new user' form

--- backend/users/forms.py
+++ backend/users/forms.py
@@ -7,7 +7,15 @@ from users.models import User
 class InviteUserForm(forms.Form):
     email = forms.CharField()
 
-    # TODO: check if the user already exists
+    def clean(self):
+        cleaned_data = super().clean()
+        email = cleaned_data["email"]
+
+        # check if email already exists
+        if User.objects.check_if_email_already_exists(email):
+            raise forms.ValidationError("The email you entered is from an existing user.")
+
+        return cleaned_data
 
 
 class SignUpForm(UserCreationForm):
