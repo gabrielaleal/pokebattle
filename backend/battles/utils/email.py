0fbc2ca43998506f132cfc1aba85d53a33962878
gabrielaleal <gabriela.leal@hotmail.com>
2020-04-07 11:31:17 -0300

    send_user_invite_to_pokebattle method

--- backend/battles/utils/email.py
+++ backend/battles/utils/email.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.urls import reverse_lazy
 
 from templated_email import send_templated_mail
 
@@ -15,5 +16,20 @@ def send_battle_result(battle):
             "battle_id": battle.id,
             "creator_team": battle.creator.teams.filter(battle=battle.id).first(),
             "opponent_team": battle.opponent.teams.filter(battle=battle.id).first(),
+        },
+    )
+
+
+def send_user_invite_to_pokebattle(user_invited_email, user_who_invited_email):
+    signup_path = reverse_lazy("signup")
+    signup_url = f"{settings.HOST}{signup_path}"
+    send_templated_mail(
+        template_name="new_user_invite",
+        from_email=settings.EMAIL_ADDRESS,
+        recipient_list=[user_invited_email],
+        context={
+            "user_who_invited": user_who_invited_email.split("@")[0],
+            "user_invited": user_invited_email.split("@")[0],
+            "signup_url": signup_url,
         },
     )
