5ceebfc110a4a6acc59b271ecf93c107b8bf1ef3
gabrielaleal <gabriela.leal@hotmail.com>
2020-04-13 13:10:15 -0300

    create battle endpoint

--- backend/api/battles/endpoints.py
+++ backend/api/battles/endpoints.py
@@ -1,11 +1,17 @@
 from django.db.models import Q
 
-from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView
+from rest_framework.permissions import IsAuthenticated
 
 from battles.models import Battle
 
 from .permissions import IsInBattle
-from .serializers import BattleSerializer
+from .serializers import BattleSerializer, CreateBattleSerializer
+
+
+class CreateBattleEndpoint(CreateAPIView):
+    serializer_class = CreateBattleSerializer
+    permission_classes = (IsAuthenticated,)
 
 
 class BattleDetailEndpoint(RetrieveAPIView):
