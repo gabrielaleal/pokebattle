12bc645fa6d3303bd6011d3042a25fd2f010ddc0
gabrielaleal <gabriela.leal@hotmail.com>
2020-04-13 13:07:51 -0300

    create battle url

--- backend/api/battles/urls.py
+++ backend/api/battles/urls.py
@@ -1,12 +1,18 @@
 from django.conf.urls import url
 
-from .endpoints import BattleDetailEndpoint, OngoingBattlesListEndpoint, SettledBattlesListEndpoint
+from .endpoints import (
+    BattleDetailEndpoint,
+    CreateBattleEndpoint,
+    OngoingBattlesListEndpoint,
+    SettledBattlesListEndpoint,
+)
 
 
 app_name = "api"
 
 urlpatterns = [
     url(r"^battle/(?P<pk>[-\w]+)/$", BattleDetailEndpoint.as_view(), name="battle-detail"),
+    url(r"^create-battle/$", CreateBattleEndpoint.as_view(), name="create-battle"),
     url(r"^settled-battles/$", SettledBattlesListEndpoint.as_view(), name="settled-battles-list"),
     url(r"^ongoing-battles/$", OngoingBattlesListEndpoint.as_view(), name="ongoing-battles-list"),
 ]
