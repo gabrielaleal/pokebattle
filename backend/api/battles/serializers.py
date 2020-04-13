2da7a8cdde930e03d6ad09a5b8797c09da06ecf3
gabrielaleal <gabriela.leal@hotmail.com>
2020-04-13 13:11:07 -0300

    create battle serializer

--- backend/api/battles/serializers.py
+++ backend/api/battles/serializers.py
@@ -7,3 +7,13 @@ class BattleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Battle
         fields = "__all__"
+
+
+class CreateBattleSerializer(serializers.Serializer):  # noqa
+    class Meta:
+        model = Battle
+        fields = "opponent"
+
+    def create(self, validated_data):
+        # TODO: attribute request.user to creator, set battle status and create battle team
+        return Battle.objects.create(**validated_data)
