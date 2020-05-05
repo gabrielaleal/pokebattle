9e1eb5be7e893027004437bf8979658a4cc47c33
gabrielaleal <gabriela.leal@hotmail.com>
2020-05-05 12:16:20 -0300

    create battle form's opponent field

--- frontend/js/pages/CreateBattle.js
+++ frontend/js/pages/CreateBattle.js
@@ -1,84 +1,115 @@
-/* eslint-disable class-methods-use-this */
-/* eslint-disable react/prefer-stateless-function */
-import { Formik } from 'formik';
+import { withFormik } from 'formik';
+import PropTypes from 'prop-types';
 import React from 'react';
 // import Yup from 'yup';
 
 import PageTitle from '../components/Title';
 import getOpponentsFromAPI from '../utils/api-helper';
 
-const SelectOpponentField = (formik) => {
-  let opponents = [];
-  let loading = true;
-  const { values, handleBlur, handleChange } = formik;
-  getOpponentsFromAPI().then((res) => {
-    opponents = res;
-    loading = false;
-    return opponents;
-  });
+class SelectOpponentField extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      opponents: [],
+    };
+  }
 
-  if (loading) return <div />;
+  componentDidMount() {
+    const { opponents } = this.state;
+    getOpponentsFromAPI().then((res) => {
+      this.setState({
+        opponents: res,
+      });
+      return opponents;
+    });
+  }
 
-  return (
-    <div>
-      {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-      <label htmlFor="email" style={{ display: 'block' }}>
-        Opponent
-      </label>
-      <select
-        name="opponent"
-        style={{ display: 'block' }}
-        value={values.opponent}
-        onBlur={handleBlur}
-        onChange={handleChange}
-      >
-        <option label="Select your opponent" value="" />
-        {opponents.map((opponent) => (
-          <option key={opponent.id} label={opponent.email} value={opponent} />
-        ))}
-      </select>
-    </div>
-  );
-};
-
-const CreateBattleForm = () => {
-  console.log('create battle form');
-  const formik = Formik({
-    mapPropsToValues: () => ({
-      opponent: '',
-    }),
-    // validationSchema: Yup.object().shape({
-    //   color: Yup.string().required('Color is required!'),
-    // }),
-    handleSubmit: (values, { setSubmitting }) => {
-      setTimeout(() => {
-        // eslint-disable-next-line no-alert
-        alert(JSON.stringify(values, null, 2));
-        setSubmitting(false);
-      }, 1000);
-    },
-  });
-
-  return (
-    <form onSubmit={formik.handleSubmit}>
-      <SelectOpponentField formik={formik} />
-    </form>
-  );
-};
-
-class CreateBattle extends React.Component {
   render() {
+    const { opponents } = this.state;
+    const { formikBag } = this.props;
+    const { values, handleChange, handleBlur, errors, touched } = formikBag;
+
     return (
-      <div className="pk-container create-battle">
-        <PageTitle title="Create Battle" />
-        {/* errors */}
-        <div className="content">
-          <div className="messages">{/* message */}</div>
-          <CreateBattleForm />
-        </div>
+      <div style={{ display: 'grid' }}>
+        <label htmlFor="opponentSelect">Opponent:</label>
+        <select
+          id="opponentSelect"
+          name="opponent"
+          value={values.opponent}
+          onBlur={handleBlur}
+          onChange={handleChange}
+        >
+          <option label="Select your opponent" value="" />
+          {opponents.map((opponent) => (
+            <option key={opponent.id} label={opponent.email} value={opponent.id} />
+          ))}
+        </select>
+        {errors.opponent && touched.opponent && <div id="feedback">{errors.opponent}</div>}
       </div>
     );
   }
 }
+
+const CreateBattleForm = (props) => {
+  const { values, touched, errors, handleChange, handleBlur, handleSubmit } = props;
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <SelectOpponentField formikBag={{ values, touched, errors, handleChange, handleBlur }} />
+      <div className="btn-container">
+        <button className="pk-btn" type="submit">
+          Submit
+        </button>
+      </div>
+    </form>
+  );
+};
+
+const CreateBattleEnhancedForm = withFormik({
+  mapPropsToValues: () => ({ opponent: {} }),
+  // Custom sync validation
+  validate: (values) => {
+    const errors = {};
+
+    if (!values.opponent) {
+      errors.opponent = 'Required';
+    }
+
+    return errors;
+  },
+  handleSubmit: (values, { setSubmitting }) => {
+    setTimeout(() => {
+      console.log(values);
+      setSubmitting(false);
+    }, 1000);
+  },
+  displayName: 'BattleForm',
+})(CreateBattleForm);
+
+const CreateBattle = () => {
+  return (
+    <div className="pk-container create-battle">
+      <PageTitle title="Create Battle" />
+      {/* errors */}
+      <div className="content">
+        {/* <div className="messages"></div> */}
+        <CreateBattleEnhancedForm />
+      </div>
+    </div>
+  );
+};
+
+CreateBattleForm.propTypes = {
+  values: PropTypes.object,
+  touched: PropTypes.object,
+  errors: PropTypes.object,
+  handleChange: PropTypes.func,
+  handleBlur: PropTypes.func,
+  handleSubmit: PropTypes.func,
+};
+
+SelectOpponentField.propTypes = {
+  formikBag: PropTypes.object,
+};
 
 export default CreateBattle;
